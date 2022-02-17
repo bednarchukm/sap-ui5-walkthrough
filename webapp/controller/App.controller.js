9f9b251ce87acd110ce843b3e13760fa71e812b9
JustSayMyName <bednarchukmv@gmail.com>
2022-02-17 22:35:05 +0300

    Step 6: Modules

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,11 +1,12 @@
 sap.ui.define([
-    'sap/ui/core/mvc/Controller'
-], function(Controller) {
+    'sap/ui/core/mvc/Controller',
+    'sap/m/MessageToast'
+], function(Controller, MessageToast) {
     'use strict';
     return Controller.extend("sap.ui.demo.walkthrough.App",{
         onShowHello : function() {
             //Show a native javascript alert
-            alert("Hello there")
+            MessageToast.show("Hello There")
         }
     });
 });
